cb74ff8f0bbe928159bd78a9823b5d9e4a07b8b5
Lina <linasoufny@gmail.com>
2020-02-21 22:05:42 -0500

    first commit

---
+++ Flashcards/Flashcards/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Lina Soufny on 2/15/20.
+//  Copyright © 2020 Lina Soufny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var backLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     frontLabel.isHidden = false
+        backLabel.isHidden = true
+    }
+
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+       /* if(){
+            
+        }
+        else{
+            
+        }*/
+        
+    }
+    
+}
+
